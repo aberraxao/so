89d71f9f58ded5d47c48cdc8c04c6f9b1875f375
dianatigre <diana_milu@live.com.pt>
2022-03-10 19:03:49 +0000

    First class exercises

---
+++ aula1/p1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Olá malta!"
+echo -n "Hora atual: "
+date
+
+# (a) Mostrar nome da directoria atual
+echo ""
+echo -n "Diretoria atual: "
+pwd
+
+# (b) Listar conteúdo da diretoria atual
+echo ""
+echo "Conteúdo: "
+ls
+echo ""
+
+echo "Adeus"
---
+++ aula1/programas/p1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Olá malta!"
+echo -n "Hora atual: "
+date
+echo "Adeus"
+echo ""
+echo -n "Diretoria atual: "
+pwd
+echo ""
+echo -n "Conteudo: "
+ls
+echo ""
